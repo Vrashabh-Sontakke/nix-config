5c1646d88a934f457a32b41f52e94d319047ab2e
Vrashabh Sontakke <vrashabhsontakke@outlook.com>
2024-09-03 16:10:31 +0530

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -7,16 +7,15 @@
   outputs = { self, nixpkgs, disko, ... }:
     let
       # TODO: Adjust these values to your needs
-#      system = "x86_64-linux";
-#      hostName = "oneclick";
+      system = "x86_64-linux";
+      hostName = "nixos";
 #      rootAuthorizedKeys = [
 #        # This user can ssh using `ssh root@<ip>`
 #        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQRxPoqlThDrkR58pKnJgmeWPY9/wleReRbZ2MOZRyd"
 #      ];
     in
     {
-#      nixosConfigurations.${hostName} = nixpkgs.lib.nixosSystem {
-      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      nixosConfigurations.${hostName} = nixpkgs.lib.nixosSystem {
         inherit system;
         modules = [
           ./disk-config.nix
