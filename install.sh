debe80193ee70e1a3376c3af0fe4412cc4788b80
Vrashabh Sontakke <vrashabhsontakke@outlook.com>
2024-09-06 01:27:14 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,9 +6,19 @@ set -x
 # Function to print usage and exit
 usage() {
     echo "Usage: $0 --flake <flake> --disk_device <disk_device>"
-    echo "  --flake <flake>        URL or reference to the flake (e.g., github:Vrashabh-Sontakke/nix-config#nixos)"
+    echo "  --flake <flake>        URL or reference to the flake (e.g., github:Vrashabh-Sontakke/nixos#nixos)"
     echo "  --disk_device <disk_device>  Disk device (e.g., /dev/nvme0n1)"
     exit 1
+}
+
+# Function to prompt for input
+prompt_for_input() {
+    if [ -z "$FLAKE" ]; then
+        read -p "Enter the flake URL or reference (e.g., github:Vrashabh-Sontakke/nixos#nixos): " FLAKE
+    fi
+    if [ -z "$DISK_DEVICE" ]; then
+        read -p "Enter the disk device (e.g., /dev/nvme0n1): " DISK_DEVICE
+    fi
 }
 
 # Parse arguments
@@ -21,14 +31,20 @@ while [[ "$#" -gt 0 ]]; do
     shift
 done
 
-# Check if FLAKE and DISK_DEVICE are provided
-if [ -z "$FLAKE" ]; then
-    echo "Error: Flake is not provided."
-    usage
+# Check if FLAKE and DISK_DEVICE are provided, if not prompt for them
+if [ -z "$FLAKE" ] || [ -z "$DISK_DEVICE" ]; then
+    if [ -z "$FLAKE" ]; then
+        echo "Flake not provided."
+    fi
+    if [ -z "$DISK_DEVICE" ]; then
+        echo "Disk device not provided."
+    fi
+    prompt_for_input
 fi
 
-if [ -z "$DISK_DEVICE" ]; then
-    echo "Error: Disk device is not provided."
+# Check if parameters are still missing after prompt
+if [ -z "$FLAKE" ] || [ -z "$DISK_DEVICE" ]; then
+    echo "Error: Both flake and disk device must be provided."
     usage
 fi
 
@@ -40,4 +56,4 @@ sudo nix \
     --write-efi-boot-entries \
     --disk main "$DISK_DEVICE"
 
-echo "Installation complete."
+echo "Script run complete."
