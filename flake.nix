812f4d2052efbb09d2ad8c59fc5ead75cdac8008
Vrashabh Sontakke <vrashabhsontakke@outlook.com>
2024-09-06 01:28:21 +0530

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,16 +1,18 @@
 {
-  description = "System Configuration";
+  description = "NixOS System Configuration";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
+
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
       ];
     };
+
   };
 }
