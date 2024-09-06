45180978fe3310c4a30f28c8e10096cc8dd2f0ec
Vrashabh Sontakke <vrashabhsontakke@outlook.com>
2024-09-06 19:16:01 +0530

    Update nvidia.nix

--- nvidia.nix
+++ nvidia.nix
@@ -23,11 +23,11 @@
   };
 
   # Enable OpenGL
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
+  #hardware.opengl = {
+    #enable = true;
+    #driSupport = true;
+    #driSupport32Bit = true;
+  #};
 
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = [
